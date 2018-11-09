6724d86ea46185ca24a624473fad98b17b9766e4
colombj <julien.colomb@fu-berlin.de>
2018-11-09 01:05:17 +0100

    new course content, output numbered

---
+++ non_website/renderall_withnewnames.R
@@ -0,0 +1,9 @@
+setwd("non_website")
+
+
+rmarkdown::render("presentation_d1.Rmd",output_file = "01_openscience.html")
+rmarkdown::render("gitbasics.Rmd",output_file = "06_git.html")
+rmarkdown::render("GSP.Rmd",output_file = "11_goodsp.html")
+rmarkdown::render("presentation_d2.Rmd",output_file = "12_reproducibility.html")
+rmarkdown::render("reproducibleanalysis_intro.Rmd",output_file = "14_repr_analysis.html")
+rmarkdown::render("OA_details.Rmd",output_file = "15_OA.html")
